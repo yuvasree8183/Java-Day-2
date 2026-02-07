2e45a305392bda330aed22747a729ee640487375
yuvasree8183 <yuvasree8183@gmail.com>
2026-02-07 11:02:20 +0530

    Add files via upload

---
+++ 24HR1A0581/Demo.java
@@ -0,0 +1,9 @@
+class Demo
+{
+    public static void main(String [] args)
+    {
+        System.out.print("Hello World");
+        System.out.println("Hi");
+        System.out.print("Good afternoon");   
+    }
+}
---
+++ 24HR1A0581/Student.java
@@ -0,0 +1,14 @@
+class Student{
+    public static void main(String [] args){
+        String name="Yuva Sree";
+        byte age=19;
+        char grade='A';
+        short marks=95;
+        String status="Pass";
+        System.out.println("Student Name:"+name);
+        System.out.println("Student Age:"+age);
+        System.out.println("Grade:"+grade);
+        System.out.println("Marks:"+marks);
+        System.out.println("Stautus of Result:"+status);
+    }
+}
