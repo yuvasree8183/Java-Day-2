687f09abc117484681065bdc7427b2a1f9d7c238
yuvasree8183 <yuvasree8183@gmail.com>
2026-02-07 11:03:06 +0530

    Update Demo.java

--- 24HR1A0581/Demo.java
+++ 24HR1A0581/Demo.java
@@ -1,9 +1,1 @@
-class Demo
-{
-    public static void main(String [] args)
-    {
-        System.out.print("Hello World");
-        System.out.println("Hi");
-        System.out.print("Good afternoon");   
-    }
-}
+
